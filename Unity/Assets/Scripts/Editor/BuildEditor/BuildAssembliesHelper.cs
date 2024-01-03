f8dfb191ce115e72a89e1cab31d4ee95e2f1df3b
CoWIN <Rhasody@outlook.com>
2024-01-03 15:55:00 +0800

    更新HybridClr

--- Unity/Assets/Scripts/Editor/BuildEditor/BuildAssembliesHelper.cs
+++ Unity/Assets/Scripts/Editor/BuildEditor/BuildAssembliesHelper.cs
@@ -4,6 +4,7 @@ using System.IO;
 using System.Linq;
 using System.Threading;
 using HybridCLR.Editor;
+using HybridCLR.Editor.Settings;
 using UnityEngine;
 using UnityEditor;
 using UnityEditor.Compilation;
